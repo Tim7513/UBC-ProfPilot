cb9584b70b32d9a9a027a465a4367ce84f1ec8cb
ExxML <andywu9509@gmail.com>
2025-08-10 22:15:04 -0700

    Fixed missing puppeteer on deployment #8

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const {join} = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, 'opt', 'render', 'project', 'src', '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, 'node_modules', 'puppeteer'),
 };
